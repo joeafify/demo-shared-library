6d1777b1b9dcf5ab5ab89cd1cbc5bef500ccfe94
Youssef Afify <joe.afify95@gmail.com>
2023-09-22 21:44:17 +0300

    Create install_nginx.groovy

---
+++ var/install_nginx.groovy
@@ -0,0 +1,22 @@
+def call(){
+  pipeline {
+    agent any
+  }
+  stages {
+    stage('Installing nginx') {
+      steps {
+        sh "sudo apt install nginx -y"
+      }
+    }
+    stage('Starting nginx service') {
+      steps {
+        sh "sudo systemctl start --now nginx"
+      }
+    }
+    stage('Request a page') {
+      steps {
+        sh "curl localhost"
+      }
+    }
+  }  
+}
