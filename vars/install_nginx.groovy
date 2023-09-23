f34ad3d81561d14793601f4f71680664bf6c560b
Youssef Afify <joe.afify95@gmail.com>
2023-09-23 11:04:53 +0300

    Removed `sudo` from install_nginx.groovy

--- vars/install_nginx.groovy
+++ vars/install_nginx.groovy
@@ -4,12 +4,12 @@ def call(){
     stages {
       stage('Installing nginx') {
         steps {
-          sh "sudo apt install nginx -y"
+          sh "apt install nginx -y"
         }
       }
       stage('Starting nginx service') {
         steps {
-          sh "sudo systemctl start --now nginx"
+          sh "systemctl start --now nginx"
         }
       }
       stage('Request a page') {
